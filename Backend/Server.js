c1040f2835ec9488163674602559b4583c1cfab8
Shubham Rahangdale <102543960+Shubhamrahangdale@users.noreply.github.com>
2026-02-24 13:09:30 +0000

    fix some minor bugs

--- Backend/Server.js
+++ Backend/Server.js
@@ -31,7 +31,7 @@ app.use(cors({
 app.use(express.json());
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const SECRET_KEY = process.env.SECRET_KEY;
 
